0b4929b05fe52b3e817c3fe271a10fd8a6e46ee8
Mrpotatosse <mrpot@DESKTOP-B4V30I0>
2024-03-16 14:29:59 +0100

    add api/user/leave

--- server/route/api/user/leave.ts
+++ server/route/api/user/leave.ts
@@ -1,10 +1,35 @@
+import { serialize } from "cookie";
 import { z } from "zod";
+import DatabaseClient from "~/server/lib/client/database";
+import { CookieKey, RequestHeader } from "~/server/lib/const/key";
+import { CookieOptions } from "~/server/lib/const/object";
+import { CustomError, CustomErrorCode } from "~/server/lib/error";
 import { apiPublicProcedure } from "~/server/lib/trpc";
+import { getAccess } from "~/server/lib/utils";
 
 const Input = z.object({});
 
 export default apiPublicProcedure.input(Input).query(async ({ ctx, input }) => {
-    return {
-        hello: "world",
-    };
+    if (!ctx.req)
+        throw new CustomError(
+            CustomErrorCode.PropertyNotFound,
+            "request not found"
+        );
+
+    const accessId = getAccess(ctx.req);
+    await DatabaseClient.access.update({
+        where: {
+            Id: accessId,
+        },
+        data: {
+            Enabled: false,
+        },
+    });
+
+    ctx.resHeaders?.append(
+        RequestHeader.SetCookie,
+        serialize(CookieKey.ACCESS, "", CookieOptions[CookieKey.ACCESS])
+    );
+
+    return {};
 });
